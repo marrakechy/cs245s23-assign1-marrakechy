f3c8937fdefe381c792d9b473585488cfabc891b
marrakechy <belmarrakechy21@coe.edu>
2023-02-04 17:54:04 -0600

    Initial commit

---
+++ src/Rational.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Rational {
+    private BigInteger numerator;
+    protected BigInteger denominator;
+
+    public Rational() {
+        this.numerator = BigInteger.ZERO;
+        this.denominator = BigInteger.ONE;
+    }
+
+}
+
